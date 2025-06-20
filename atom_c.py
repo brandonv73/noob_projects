1c9142b2ca33c990a26fca82c4ec972a3efecfdf
brandonv73 <107486416+brandonv73@users.noreply.github.com>
2025-06-20 16:14:51 -0300

    Add files via upload

---
+++ atom_c.py
@@ -0,0 +1,33 @@
+import psutil
+import smtplib
+from email.mime.text import MIMEText
+#Paso 1: Verificar el espacio en C:
+def verificar_espacio(drive="C:\\"):
+    uso = psutil.disk_usage(drive)
+    porcentaje_libre = 100 - uso.percent
+    return porcentaje_libre
+#Paso 2: Enviar correo
+def enviar_alerta(destinatario, mensaje):
+    remitente = "braanvergara@gmail.com"
+    contraseña = "vnjn qxsv uujt iici" #Clave app segura
+
+    msg = MIMEText(mensaje)
+    msg["Subject"] = "Alerta: Espacio en disco bajo"
+    msg["From"] = remitente
+    msg["To"] = destinatario
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as servidor:
+        servidor.login(remitente, contraseña)
+        servidor.send_message(msg)
+
+#Paso 3: Integracion.
+
+if __name__ == "__main__":
+    umbral = 20 #Porcentaje de alerta
+    espacio = verificar_espacio()
+
+    if espacio < umbral:
+        mensaje = f"Atencion: Solo queda {espacio: .2f}% de espacio libre en C:."
+        enviar_alerta("brandonjvergara.18@gmail.com", mensaje)
+    else:
+        print(f"Espacio suficiente: {espacio: .2f}% libre en C:.")
---
+++ logs.py
@@ -0,0 +1,51 @@
+import os
+from collections import Counter
+from datetime import datetime
+
+# Palabras a buscar
+CLAVES = ["ERROR", "WARNING", "Timeout"]
+
+def buscar_logs(directorio):
+    return [f for f in os.listdir(directorio) if f.endswith(".log")]
+
+def analizar_log(ruta_carpeta):
+    conteo = Counter()
+    eventos_relev = []
+
+    logs = buscar_logs(ruta_carpeta)
+    print(f"Archivos encontrados: {logs}")
+
+    for log in logs:
+        ruta = os.path.join(ruta_carpeta, log)
+        with open(ruta, "r", encoding="utf-8", errors="ignore") as archivo:
+            for line in archivo:
+                for clave in CLAVES:
+                    if clave.lower() in line.lower():
+                        conteo[clave] += 1
+                        eventos_relev.append(f"[{datetime.now().strftime('%Y-%m-%d')}] {log}: {line.strip()}")
+                        break
+
+    return conteo, eventos_relev
+
+def guardar_results(eventos, carpeta_salida="C:\\Users\\Brand\\PycharmProjects\\Learning\\Scripts\\log reports"):
+    os.makedirs(carpeta_salida, exist_ok=True)
+    fecha = datetime.now().strftime("%d-%m-%Y")
+    nombre_archivo = f"logs_filters_{fecha}.txt"
+    ruta_salida = os.path.join(carpeta_salida, nombre_archivo)
+
+    with open(ruta_salida, "w", encoding="utf-8") as f:
+        f.write(f"Analisis ejecutado: {datetime.now()}\n")
+        f.write("="*40 + "\n")
+        for line in eventos:
+            f.write(line + "\n")
+
+if __name__ == "__main__":
+    carpeta_logs = "C:\\Windows\\System32\\winevt\\Logs"
+
+    conteo, eventos = analizar_log(carpeta_logs)
+    guardar_results(eventos)
+
+    print("Resumen de eventos encontrados:")
+    for clave, cant in conteo.items():
+        print(f" - {clave}: {cant}")
+
